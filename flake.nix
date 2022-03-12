a2f0070ae6d9808658518c4d51f7d94fe2b6e7d7
hugosenari <hugosenari@gmail.com>
2022-03-12 04:13:55 -0300

    chore: re index nim-snappy

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim implementation of Snappy compression algorithm'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nim-snappy-master".url = "path:./master";
+  inputs."nim-snappy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim-snappy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim implementation of Snappy compression algorithm'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nim-snappy-master.flake = false;
+  inputs.src-nim-snappy-master.owner = "status-im";
+  inputs.src-nim-snappy-master.ref   = "refs/heads/master";
+  inputs.src-nim-snappy-master.repo  = "nim-snappy";
+  inputs.src-nim-snappy-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nim-snappy-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nim-snappy-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
