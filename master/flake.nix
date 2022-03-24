bf64bd26b77278fe42423082afadd29e63f81dda
hugosenari <hugosenari@gmail.com>
2022-03-23 22:41:48 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Nim implementation of Snappy compression algorithm'';
+  description = ''Nim implementation of snappy compression algorithm'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -7,19 +7,35 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-nim-snappy-master.flake = false;
-  inputs.src-nim-snappy-master.ref   = "refs/heads/master";
-  inputs.src-nim-snappy-master.owner = "status-im";
-  inputs.src-nim-snappy-master.repo  = "nim-snappy";
-  inputs.src-nim-snappy-master.type  = "github";
+  inputs.src-snappy-master.flake = false;
+  inputs.src-snappy-master.ref   = "refs/heads/master";
+  inputs.src-snappy-master.owner = "status-im";
+  inputs.src-snappy-master.repo  = "nim-snappy";
+  inputs.src-snappy-master.type  = "github";
+  
+  inputs."faststreams".owner = "nim-nix-pkgs";
+  inputs."faststreams".ref   = "master";
+  inputs."faststreams".repo  = "faststreams";
+  inputs."faststreams".dir   = "";
+  inputs."faststreams".type  = "github";
+  inputs."faststreams".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."faststreams".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."stew".owner = "nim-nix-pkgs";
+  inputs."stew".ref   = "master";
+  inputs."stew".repo  = "stew";
+  inputs."stew".dir   = "";
+  inputs."stew".type  = "github";
+  inputs."stew".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stew".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nim-snappy-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-snappy-master"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-nim-snappy-master";
+    src  = deps."src-snappy-master";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
